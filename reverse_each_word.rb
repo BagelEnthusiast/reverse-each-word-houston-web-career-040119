d391b214911ead40d80430bf8cbfc4d3b4b9a660
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-27 11:37:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   newSentence = sentence1.split
   testSentence = ""
   newSentence.collect do |string|
-    testSentence << "#{string[0..-1].reverse} "
+    testSentence << "#{string[0..-2].reverse} "
   end
   testSentence
 end
