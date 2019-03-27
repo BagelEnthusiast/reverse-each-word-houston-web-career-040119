b35ea9a68feeb24d0689936754c534bb853c2ab2
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-27 11:05:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
-  sentence1.split
-  sentence1.collect do |string|
+  newSentence = sentence1.split
+  newSentence.each do |string|
     string.reverse 
   end 
 end
